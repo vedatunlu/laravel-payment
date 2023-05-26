8d92529991a9aadf985bc18fdb8e19bd132156ed
Vedat Ünlü <vedatunlu10@gmail.com>
2023-05-26 14:44:03 +0300

    config file refactored

--- config/payment.php
+++ config/payment.php
@@ -10,11 +10,13 @@ return [
     |
     */
 
-    'credentials' => [
-        'host' => env('SIPAY_HOST', null),
-        'merchant_id' => env('SIPAY_MERCHANT_ID', null),
-        'merchant_key' => env('SIPAY_MERCHANT_KEY', null),
-        'app_key' => env('SIPAY_APP_KEY', null),
-        'app_secret' => env('SIPAY_APP_SECRET', null),
+    'sipay' => [
+        'credentials' => [
+            'host' => env('SIPAY_HOST', null),
+            'merchant_id' => env('SIPAY_MERCHANT_ID', null),
+            'merchant_key' => env('SIPAY_MERCHANT_KEY', null),
+            'app_key' => env('SIPAY_APP_KEY', null),
+            'app_secret' => env('SIPAY_APP_SECRET', null),
+        ]
     ]
 ];
